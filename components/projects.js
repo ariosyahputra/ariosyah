5ada943ab1f191091033cc3ce49935bb275ea2e2
ariosyahputra <ariosyh@gmail.com>
2024-02-09 22:46:30 +0700

    code optimization, more projects added

---
+++ components/projects.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Add the 'loaded' class to the body after a short delay
+    setTimeout(function () {
+        document.body.classList.add('loaded');
+    }, 500); // You can adjust the delay (in milliseconds) as needed
+});
+
